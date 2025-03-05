60ef7ce21b8a91aab0297fdb661d5b1da3b453e2
DeadKesar <50805926+DeadKesar@users.noreply.github.com>
2025-03-05 13:16:07 +0300

    финал финал

--- app/src/main/java/com/example/moblab2/MainActivity.kt
+++ app/src/main/java/com/example/moblab2/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.moblab2
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
-
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.CoroutineScope
